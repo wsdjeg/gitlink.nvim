db0e3e65f5581b8c5c7fc762e73de1a6419dd865
Eric Wong <wsdjeg@outlook.com>
2026-02-24 14:35:40 +0800

    fix: skip for no git repository

--- lua/gitlink/init.lua
+++ lua/gitlink/init.lua
@@ -1,5 +1,7 @@
 local M = {}
 
+local util = require('gitlink.util')
+
 local function remote_url_to_http(url)
     if vim.startswith(url, 'git@') then
         return 'https://' .. vim.fn.substitute(string.sub(url, 5, -5), ':', '/', 'g')
@@ -14,14 +16,17 @@ end
 
 local function get_url()
     if vim.fn.executable('git') == 0 then
-        require('gitlink.util').notify('git is not executable!')
+        util.notify('git is not executable.')
         return
     end
     local find_path = vim.fs.root(0, '.git')
+    if not find_path then
+        util.notify('not a git repository.')
+        return
+    end
     local remove_verbose_info = vim.tbl_filter(function(t)
         return vim.regex('(fetch)$'):match_str(t)
     end, vim.split(vim.system({ 'git', 'remote', '-v' }, { text = true }):wait().stdout, '\n'))[1]
-    local remote = vim.fn.split(remove_verbose_info)[1]
     local url = vim.fn.split(remove_verbose_info)[2]
     url = remote_url_to_http(url)
     return url
